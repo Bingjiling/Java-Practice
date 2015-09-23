d2de110a12097dbb28ff9fb7dd49da10649bd380
Yanyu Zheng <yz2690@columbia.edu>
2015-09-23 16:28:06 -0400

    homework 1

--- 1/src/Range.java
+++ 1/src/Range.java
@@ -3,19 +3,24 @@
  */
 public class Range implements Iterable<Integer> {
 	// you probably need some variables here
-
+    SimpleLinkedList<Integer> lst;
+    
 	public Range(int min, int max, int increment) {
-		// change this
+		for (int i = min; i < max; i=i+increment){
+			lst.add(i);
+		}
 	}
 
 	public Range(int min, int max) {
-		// change this
+		for (int i = min; i < max; i++){
+			lst.add(i);
+		}
 	}
 
 	public java.util.Iterator<Integer> iterator() {
 		// change this
 		// also understand what this actually does and the easiest way to do this
 		// should be a one liner
-		return null;
+		return lst.iterator();
 	}
 }
